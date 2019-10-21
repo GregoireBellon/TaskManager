c438fa8903a206c881af791b0eb5f6bddae23aa6
Nathan Le Moan <lemoannathan@gmail.com>
2019-10-21 16:31:14 +0200

    Ajout de la page de création de compte (pas encore testée)

--- Scripts/verifSignIn.php
+++ Scripts/verifSignIn.php
@@ -1,4 +1,37 @@
 <?php
     // Cette classe permet de vérifier que le formulaire de création de compte a été rempli correctement.
     // Il ajoute également cet utilisateur à la base de données.
+    session_start();
+    require("../Classes/ManipBDD.php");
+    $bdd = new ManipBDD();
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $password_confirm = $_POST["password_confirm"];
+
+    if ($username == null or $username == ""){
+        $_SESSION["username_error"] = "Veuillez entrer un nom d'utilisateur";
+        header("Location: ../View/pageSignin.php");
+    }
+
+    elseif($password != $password_confirm){
+        $_SESSION["password_not_same"] = "Les mots de passe ne sont pas identiques";
+        header("Location: ../View/pageSignin.php");
+    }
+
+    elseif (!$bdd->verifUniciteUser($username)){
+        $_SESSION["username_error"] = "Nom d'utilisateur non disponible";
+        header("Location: ../View/pageSignin.php");
+    }
+
+    else{
+        $bdd->ajouterUtilisateur($username, $password);
+        $_SESSION["id_user"] = $bdd->getIdUser($username);
+        $_SESSION["username"] = $username;
+        $_SESSION["password"] = "";
+        $_SESSION["username_error"] = "";
+        $_SESSION["password_not_same"] = "";
+        header("Location: ../View/pageAccueil.php");
+    }
+
+
 ?>
